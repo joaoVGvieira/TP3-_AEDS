1e83507fadf07c8d24cd3c7e2b6012f2e33ae675
joaoVGvieira <joaovitorgv2019@gmail.com>
2022-03-13 17:43:55 -0300

    novo

--- inteligente.c
+++ inteligente.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-void Ordena(int inicio, int fim, int *vet)
+void Ordena(int inicio, int fim, int *vet, int cont)
 {
-    int i,j,menor;    
+    int i,j,menor=0;    
     Particao(inicio, fim, &i, &j, vet);
-    menor=compara(inicio,fim,vet); 
-    printf("menor==%d\n",menor);
+    if (cont==0){
+        menor=compara(inicio,fim,vet); 
+    }
     if (menor==0)
     {
-        if (inicio < j) Ordena(inicio, j, vet);
+        if (inicio < j) 
+        {
+            Ordena(inicio, j, vet,1);
+        }
+        menor++;
     }
-    if (menor==1)
+    if(menor==1)
     {
-        if (i < fim) Ordena(i, fim, vet); 
+        if (i < fim) 
+        {
+            Ordena(i, fim, vet,1); 
+        }
     }
 }
 void QuickSort(int *vet, int n)
 {
-    Ordena(0, n-1, vet);
+    Ordena(0, n-1, vet,0);
 }
 
 // ESSA FUNÇAO VAI COMPARAR O MENOR LADO ANTES OU DEPOIS DO PIVO
@@ -28,25 +36,26 @@ int compara(int inicio, int fim, int *vet){
     j = fim;
     media=(i + j)/2;
     pivo = vet[media];
-    //printf("pivor==%d\n",media);
     for ( int k = 0; k < media; k++)
     {
-        cont_esq++;
+        cont_esq+=1;
 
     }
     for ( int h = fim; h > media; h--)
     {
-        cont_dir++;
- 
+        cont_dir+=1;
     }
-    if (cont_esq <= cont_dir)
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
+    printf("esq=%d\ndir=%d\n",cont_esq,cont_dir);
+   if (cont_esq <= cont_dir )
+   {
+       return 0;
+   }
+   else
+   {
+       return 1;
+   }
+   
+   
 }
  
 void Particao(int inicio, int fim,int *i, int *j, int *vet){
@@ -74,7 +83,7 @@ void Particao(int inicio, int fim,int *i, int *j, int *vet){
 }
 int main(){
     int i;
-    int vet[] = {8,3,15,6,7,4};
+    int vet[] = {8,3,15,6,7,4,2,1};
     size_t N = sizeof(vet)/sizeof(vet[0]);
     QuickSort(&vet,N);
     printf("\nVetor ordenado:\n");
